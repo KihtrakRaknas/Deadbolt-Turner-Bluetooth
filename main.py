1f2d6e17667e2d10beba76d3fa507c3d5e2020c0
KihtrakRaknas <karthikandsankar@gmail.com>
2024-08-03 19:25:01 -0400

    Fork code to work with deadbolt 3D model
    Added open and close end points.
    
    Simplified bluetooth presence detection.
    
    Removed command line arguments in favor of a .env file.
    Added a template file for the .env file.
    
    Opted for pipenv instead of a requirements.txt file.
    
    Updated setup instructions in the readme.
    
    Switched to gpiozero library with PiGPIO for simplity and improved performance.

--- main.py
+++ main.py
@@ -1,182 +1,125 @@
 #!/usr/bin/env python3
+import os
+from dotenv import load_dotenv
 from bt_proximity import BluetoothRSSI
-import time
-import sys, getopt
 import requests_async as requests
 import RPi.GPIO as gpio  # https://pypi.python.org/pypi/RPi.GPIO more info
-import atexit
 import asyncio
 import threading
-import datetime
 import os
 from flask import Flask, request
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Device, AngularServo
+Device.pin_factory = PiGPIOFactory()
 app = Flask(__name__)
 
-# Stepper motor constants
-STEPTIME = 0.0002
-STEPS = 1960
-PRIMESTEPS = 1500
-SLEEPPIN = 25
-STEPPIN = 24
-DIRPIN = 23
-CLOCKWISE = True
-RESETTIME = .4
-HOLDTIME = 20
+load_dotenv()
+BT_ADDR = os.getenv('BT_ADDR')
+NOTI_PROJ = os.getenv('NOTI_PROJ', "")
+PASSWORD = os.getenv('PASSWORD', "")
+OPEN_ANGLE = int(os.getenv('OPEN_ANGLE', "45"))
+CLOSE_ANGLE = int(os.getenv('CLOSE_ANGLE', "-45"))
 
 # Bluetooth constants
-HISTORYLENGTH = 200
-CLOSELENGTH = 4
-CLOSETHRESH = 3
-FARLENGTH = 40
-FARTHRESH = 12
+HISTORYLENGTH = int(os.getenv('TAPE_LENGTH', "200"))
+CLOSELENGTH = int(os.getenv('CLOSENESS_DELAY', "4"))
+CLOSETHRESH = int(os.getenv('CLOSENESS_THRESH', "3"))
 
-# iphone 13 pro: 'F8:C3:CC:9C:C9:6C'
+# The MG90S spec sheet indicates that the min is .001 and the max is .002.
+# Using .0005 and .0025 as the max and min results in the expected 180 degrees
+# of motion.
+door_servo = AngularServo(14, max_pulse_width=2.5/1000, min_pulse_width=.5/1000, 
+                          initial_angle=None)
 
-motor_lock = asyncio.Lock()
-
-notibot_project = ''
-password = ''
-guestpassword = ''
 
 async def main():
-    BT_ADDR = ''
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hb:n:p:g:", ["bluetoothaddress=", "notibotproject=", "password=","guestpassword="])
-    except getopt.GetoptError:
-        print('USAGE: main.py -b <bluetoothaddress> [-n <notibotproject>] [-p <password>]')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('USAGE: main.py -b <bluetoothaddress> [-n <notibotproject>] [-p <password>]')
-            sys.exit()
-        elif opt in ("-b", "--bluetoothaddress"):
-            BT_ADDR = arg
-        elif opt in ("-n", "--notibotproject"):
-            global notibot_project
-            notibot_project = arg
-        elif opt in ("-p", "--password"):
-            global password
-            password = arg
-        elif opt in ("-g", "--guestpassword"):
-            global guestpassword
-            guestpassword = arg
-    if BT_ADDR == '':
-        print('USAGE: main.py -b <bluetoothaddress>')
-        sys.exit()
-
     global loop
     loop = asyncio.get_event_loop()
-    set_up_motor()
     t1 = threading.Thread(target=server)
     t1.start()
-    # asyncio.create_task(open_door())
-    # await asyncio.sleep(1000)
+
     btrssi = BluetoothRSSI(addr=BT_ADDR)
-    old_rssi = None
     old_rssis = []
-    delete_me = False
+    
+    def add_rssi(rssi_val):
+        old_rssis.append(rssi_val)
+        if len(old_rssis) > HISTORYLENGTH:
+            old_rssis.pop(0)
+    
+    device_present = None
     while True:
         rssi = btrssi.request_rssi()
         print(rssi)
-        if rssi is None:
-            old_rssis = [-100]*HISTORYLENGTH
-            if not (old_rssi is None):
-                old_rssi = None
-                # asyncio.create_task(notif_call("phone%20lost"))
-                delete_me = False
-        else:
-            rssi = rssi[0]
+        if rssi is not None:
+            add_rssi(rssi[0])
+                
+            if all(el is None for el in old_rssis):
+                if device_present != False:
+                    device_present = False
+                    asyncio.create_task(close_door())
 
-            old_rssis.append(rssi)
-            if len(old_rssis) > HISTORYLENGTH:
-                old_rssis.pop(0)
-
-            if old_rssi is None:
-                old_rssi = rssi
-                # asyncio.create_task(notif_call("phone%20detected"))
-            else:
-                print(str(old_rssis[:FARLENGTH]))
-                print(str(all(-CLOSETHRESH <= el <= 0 for el in (old_rssis[-CLOSELENGTH:]))) + " & " + str(not any(-FARTHRESH <= el <= 0 for el in (old_rssis[:FARLENGTH]))))
-                if any(-FARTHRESH <= el <= 0 for el in (old_rssis[:FARLENGTH])) and not delete_me:
-                    delete_me = True
-                    # asyncio.create_task(notif_call("Too late: "+str(old_rssis)))
-
-                if all(-CLOSETHRESH <= el <= 0 for el in (old_rssis[-CLOSELENGTH:])) and not any(-FARTHRESH <= el <= 0 for el in (old_rssis[:FARLENGTH])):
-                    old_rssis = [0]*HISTORYLENGTH
+            if all(-CLOSETHRESH <= el <= 0 for el in old_rssis[-CLOSELENGTH:]):
+                if device_present != True:
+                    device_present = True
                     asyncio.create_task(open_door())
+        else:
+            add_rssi(None)
+            
         await asyncio.sleep(1)
 
 
 async def open_door():
     print("open door called")
-    if motor_lock.locked():
-        return
-    async with motor_lock:
-        def init():
-            gpio.output(SLEEPPIN, True) # Stop the motor from sleeping
-            gpio.output(DIRPIN, CLOCKWISE) # Set the direction
-            
-        def step(steps):
-            stepCounter = 0
-            while stepCounter < steps:
-                # Tell the easy driver to take one step
-                gpio.output(STEPPIN, True)
-                gpio.output(STEPPIN, False)
-                stepCounter += 1
+    asyncio.create_task(notif_call("opened door"))
+    door_servo.angle = OPEN_ANGLE
+    await asyncio.sleep(3)
+    door_servo.detach()
     
-                # Wait before taking the next step...this controls rotation speed
-                time.sleep(STEPTIME)
+    
+async def close_door():
+    print("close door called")
+    asyncio.create_task(notif_call("closed door"))
+    door_servo.angle = CLOSE_ANGLE
+    await asyncio.sleep(3)
+    door_servo.detach()
 
-        async def sleep():
-            gpio.output(25, False)  # Motor goes back to sleeping
-            await asyncio.sleep(RESETTIME)
-            
-        asyncio.create_task(notif_call("opened%20door"))
-        
-        init()
-        step(PRIMESTEPS)
-        await sleep()
-        # init()
-        # step(PRIMESTEPS)
-        # await sleep()
-        init()
-        step(STEPS)
-        await asyncio.sleep(HOLDTIME)
-        await sleep()
-        
-        # asyncio.create_task(notif_call("close%20door"))
 
+def valid_password(request):
+    valid = False
+    if PASSWORD == "":
+        valid = True
+    if request.method == "GET" and request.args['password'] == PASSWORD:
+        valid = True
+    if request.method == "POST":
+        if request.json["password"] == PASSWORD:
+            valid = True
+    return valid
 
 def server():
-    @app.route('/webhook', methods=['GET', 'POST'])
+    @app.route('/open', methods=['GET', 'POST'])
     def respond():
-        print("webhook called")
-        valid = False
-        if password == "":
-            valid = True
-        if request.method == "GET" and request.args['password'] == password:
-            valid = True
-        if request.method == "POST":
-            if request.json["password"] == password:
-                valid = True
-            if guestpassword!= "" and request.json["password"] == guestpassword:
-                now = datetime.datetime.now()
-                # Monday is 0
-                if (1 == now.weekday() or 3 == now.weekday()) and datetime.time(hour=8, minute=30) <= now.time() <= datetime.time(hour=9, minute=30):
-                    valid = True
-                notif_call('Guest%20Door%20Open%20Request')#&body=Valid%3A%20'+str(valid))
-                if not valid:
-                    return "Guest password not allowed at this time"
-        if valid:
+        print("open called")
+        if valid_password(request):
             asyncio.run_coroutine_threadsafe(open_door(), loop)
             return 'Done'
         return 'Invalid Password'
+    
+    
+    @app.route('/close', methods=['GET', 'POST'])
+    def respond():
+        print("close called")
+        if valid_password(request):
+            asyncio.run_coroutine_threadsafe(close_door(), loop)
+            return 'Done'
+        return 'Invalid Password'
+
 
     @app.route('/reboot', methods=['GET', 'POST'])
     def reboot():
-        if request.json["password"] != password:
-            return "Invalid Password"
-        os.system('sudo shutdown -r now')
+        if valid_password(request):
+            os.system('sudo shutdown -r now')
+            return 'Done'
+        return "Invalid Password"
     from waitress import serve
     
     serve(app, host="0.0.0.0", port=8080)
@@ -184,29 +127,9 @@ def server():
 
 
 async def notif_call(str):
-    if notibot_project != '':
-        await requests.get('https://n.kihtrak.com/?project='+notibot_project+'&title='+str)
+    if NOTI_PROJ != '':
+        await requests.get(f"https://n.kihtrak.com/?project={NOTI_PROJ}&title={str}")
 
-
-def set_up_motor():
-    # use the broadcom layout for the gpio
-    gpio.setmode(gpio.BCM)
-    # GPIO23 = Direction
-    # GPIO24 = Step
-    # GPIO25 = SLEEP
-    gpio.setup(23, gpio.OUT)
-    gpio.setup(24, gpio.OUT)
-    gpio.setup(25, gpio.OUT)
-
-    gpio.output(25, False) # Make the motor sleep
-
-
-def exit_handler():
-    gpio.cleanup()
-
-
-atexit.register(exit_handler)
 
 if __name__ == '__main__':
     asyncio.run(main())
-
