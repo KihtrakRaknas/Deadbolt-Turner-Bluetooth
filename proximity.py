1c5a3b167c8c0cac3a6046d0be8309f8e4985447
KihtrakRaknas <karthikandsankar@gmail.com>
2024-11-19 01:25:27 -0500

    Auto-retry if we get BTLEDisconnectError

--- proximity.py
+++ proximity.py
@@ -1,5 +1,6 @@
+import time
 from scanner_patch import ScannerWStop
-from bluepy.btle import DefaultDelegate
+from bluepy.btle import DefaultDelegate, BTLEDisconnectError
         
 class MyDelegate(DefaultDelegate):
     def __init__(self, target_address, scanner):
@@ -29,7 +30,16 @@ def rssi_to_distance(rssi, calibrated_rssi=-50):
     return 10 ** ((calibrated_rssi - rssi) / (10 * 2))
 
 def get_distance_to_device(target_address, timeout=30):
-    rssi = get_device_rssi(target_address, timeout=timeout)
+    max_retries = 2
+    for attempt in range(max_retries):
+        try:
+            rssi = get_device_rssi(target_address, timeout=timeout)
+            break
+        except BTLEDisconnectError:
+            if attempt == max_retries - 1:
+                raise
+            time.sleep(5)
+            continue
     if rssi is not None:
         return rssi_to_distance(rssi)
     return None
